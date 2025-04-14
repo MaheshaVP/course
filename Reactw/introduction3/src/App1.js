c6150149b3776b6e12ca7b70d21300347a91a641
Mahesha V P <maheshavp12@gmail.com>
2025-04-14 23:50:46 +0530

    react

---
+++ Reactw/introduction3/src/App1.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Todo from "./Todo";
+import React from "react";
+
+const App1 = () => {
+    const [count, setCount] = useState(0);
+    const [todos, setTodos] = useState(["todo 1", "todo 2"]);
+  
+    const increment = () => {
+      setCount((c) => c + 1);
+    };
+  
+    return (
+      <>
+        <Todo todos={todos} />
+        <hr />
+        <div>
+          Count: {count}
+          <button onClick={increment}>+</button>
+        </div>
+      </>
+    );
+  };
+  
+  export default App1;
