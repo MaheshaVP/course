ab99d5a5a1e46df1a3fd71c08ae721ae03cacc79
Mahesha V P <maheshavp12@gmail.com>
2025-10-24 23:32:05 +0530

    react

--- practice/React/basic/first-app/src/App.jsx
+++ practice/React/basic/first-app/src/App.jsx
@@ -3,8 +3,10 @@ import './App.css'
 function App() {
   return(
     <div className='App'>
-      <h1>Hello world</h1>
+      <h1>Hello world</h1><br/>
+      <h2>Good Morning</h2>
     </div>
+    
   );
 }
 
